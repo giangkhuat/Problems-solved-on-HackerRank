ce7328a16edfaead6e7d5613ce0ca519be49fefc
giangkhuat <khuatgia@grinnell.edu>
2019-08-23 22:22:35 -0400

    Add more tree problems

---
+++ Solutions/src/checkBST.java
@@ -0,0 +1,29 @@
+
+/*
+ * Problem: Check if a binary Tree is a BST Tree
+ * Constraints: 0 < data < 10^4
+ * Assumption: No duplicate in the tree
+ * Time Complexity: O(n)
+ */
+
+public class checkBST {
+  public boolean isBSTHelper(Node root, int min, int max) {
+    if (root == null) {
+      return true;
+    } else if (root.data > min && root.data < max && isBSTHelper(root.left, min, root.data)
+        && isBSTHelper(root.right, root.data, max)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean checkBSTFun(Node root) {
+    return isBSTHelper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+}
+
+/*
+ * Non-recursive solution: if root.data is an object
+ * Solution using stack and inorder traversal concept, checking if the stack is already sorted
+*/
